c473f14a1b54047ee344062c17220e715ccd3d2a
aryark <aryark3112@gmail.com>
2023-07-12 11:15:51 +0530

    first commit

--- Program.cs
+++ Program.cs
@@ -57,8 +57,8 @@ namespace loops
             //{
             //    Console.WriteLine("Invalid user.");
             //}
-
             //Console.ReadLine();
+
             //var myNum = 12;
             //var myString = "Sam";
             //var status = true;
@@ -68,6 +68,7 @@ namespace loops
             //Console.WriteLine("Value Stored in status is: " + status + "\n Data Type :" + status.GetType());
             //Console.WriteLine("Value Stored in grade is: " + grade + "\n Data Type :" + grade.GetType());
             //Console.ReadKey();
+
             //var myNum = 12;
             //dynamic myDynamic = 45;
             //Console.WriteLine("Value Stored in myDynamic is: " + myDynamic + "\n DataType Stored is:\t" + myDynamic.GetType());
@@ -77,6 +78,7 @@ namespace loops
             //Console.WriteLine("Value Stored in myDynamic is: " + myDynamic + "\n DataType Stored is:\t" + myDynamic.GetType());
             //Console.WriteLine("Value Stored in myNum is: " + myNum + "\n Data Type :" + myNum.GetType());
             //Console.ReadKey();
+
             //try
             //{
             //int num1, num2,result;
@@ -93,22 +95,34 @@ namespace loops
             // Console.ReadKey();
             // Console.WriteLine();
             //}
-            try
-            {
-                Console.WriteLine("Please enter a number");
-                string input = Console.ReadLine();
-                if (int.TryParse(input, out int result))
-                {
-                    Console.WriteLine("Parsed Number using intTryParse is: \t" + result);
-                }
-                else
-                {
-                    Console.WriteLine("Failed tp Parse");
-                }
-            }
-            catch (Exception ex)
-            { Console.WriteLine("Error!!" + ex.Message); }
-            finally { Console.ReadKey(); }
-            }
+
+            //try parse
+
+            //try
+            //{
+            //Console.WriteLine("Please enter a number");
+            //string input = Console.ReadLine();
+            //if (int.TryParse(input, out int result))
+            //{
+            //   Console.WriteLine("Parsed Number using intTryParse is: \t" + result);
+            //}
+            //else
+            //{
+            //   Console.WriteLine("Failed tp Parse");
+            //}
+            //}
+            //catch (Exception ex)
+            //{ Console.WriteLine("Error!!" + ex.Message); }
+            //finally { Console.ReadKey(); }
+
+            double num1, num2, result;
+            Console.WriteLine("Enter First Number");
+            double.TryParse(Console.ReadLine(), out num1);
+            Console.WriteLine("Enter Second Number");
+            double.TryParse(Console.ReadLine(), out num2);
+            result = num1 / num2;
+            Console.WriteLine($"Result after dividing {num1} by {num2} is= \t {result}");
+            Console.ReadKey();
+        }
     }
 }
